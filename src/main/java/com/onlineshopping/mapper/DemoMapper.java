ea9b2a2913ea91f10e0291fc81ab98c64a41d838
Lin-Yanjun <20307130136@fudan.edu.cn>
2023-03-23 20:58:09 +0800

    lyj-1: rm Demo

--- src/main/java/com/onlineshopping/mapper/DemoMapper.java
+++
@@ -1,9 +0,0 @@
-package com.onlineshopping.mapper;
-
-import com.onlineshopping.model.entity.User;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DemoMapper {
-    User selectUserById(Integer id);
-}
--- src/main/java/com/onlineshopping/service/DemoService.java
+++
@@ -1,8 +0,0 @@
-package com.onlineshopping.service;
-
-import com.onlineshopping.exception.ServiceException;
-import com.onlineshopping.model.dto.DemoDTO;
-
-public interface DemoService {
-    DemoDTO getUserById(Integer id) throws ServiceException;
-}
