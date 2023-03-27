32cc62929365edd633301af820a05c41f9666b7a
zhw <3203393071@qq.com>
2023-03-27 18:18:42 +0800

    wzh-1 实现退出

--- webapp/shop/src/components/Home.vue
+++ webapp/shop/src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        Home 组件
+        <el-button type="info" @click="logout">退出</el-button>
     </div>
 </template>
 
@@ -8,7 +8,12 @@
 
 
 export default {
-   
+   methods:{
+    logout(){
+        window.sessionStorage.clear();
+        this.$router.push("/user/login");
+    }
+   }
 }
 </script>
 
