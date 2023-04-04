6122878e8da3beae39c27c1b08eaeb76a73a9eb8
Lin-Yanjun <20307130136@fudan.edu.cn>
2023-04-04 12:07:26 +0800

    lyj-1: rm redundant throws (java will add by default)

--- src/main/java/com/onlineshopping/service/UserService.java
+++ src/main/java/com/onlineshopping/service/UserService.java
@@ -11,23 +11,23 @@ public interface UserService {
      * @Description: 检查用户注册信息。若都有效就将用户插入数据库
      * @Author: Lin-Yanjun
      */
-    void register(UserRegisterDTO user) throws RuntimeException;
+    void register(UserRegisterDTO user);
 
     /**
      * @Description: 检查用户登录的用户名和密码，若匹配则返回token
      * @Author: Lin-Yanjun
      */
-    String login(HttpServletRequest request, HttpServletResponse response, UserLoginDTO userLoginDTO) throws RuntimeException;
+    String login(HttpServletRequest request, HttpServletResponse response, UserLoginDTO userLoginDTO);
 
     /**
      * @Description: 啥也不做
      * @Author: Lin-Yanjun
      */
-    void logout(HttpServletRequest request, HttpServletResponse response) throws RuntimeException;
+    void logout(HttpServletRequest request, HttpServletResponse response);
 
     /**
      * @Description: 获取用户详细信息
      * @Author: Lin-Yanjun
      */
-    UserInfoVO info(HttpServletRequest request, HttpServletResponse response) throws RuntimeException;
+    UserInfoVO info(HttpServletRequest request, HttpServletResponse response);
 }
