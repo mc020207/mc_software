7f3e064a193eae253e8a5fee9bf9723fc54d6156
zhw <3203393071@qq.com>
2023-05-01 20:02:02 +0800

    wzh :1

--- webapp/shop/src/components/user/UserInfo.vue
+++ webapp/shop/src/components/user/UserInfo.vue
@@ -97,20 +97,6 @@
       </span>
     </el-dialog>
 
-<el-upload
-  class="upload-demo"
-  action="http://localhost:8081/front/owner/product/image/add"
-  :on-preview="handlePreview"
-  :on-remove="handleRemove"
-  :before-remove="beforeRemove"
-  multiple
-  :limit="3"
-  :on-exceed="handleExceed"
-  :headers="headersOBJ"
-  :file-list="fileList">
-  <el-button size="small" type="primary">点击上传</el-button>
-  <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
-</el-upload>
   </div>
 </template>
 
@@ -120,10 +106,6 @@ export default {
   data() {
     return {
       //修改信息
-       fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}],
-        headersOBJ:{
-          Authorization:"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyUHdkIjoiODU0YjZjZWUxZjQzODdmYzFlYzg5NDI3ZWYxOWRkM2QiLCJ1c2VyUm9sZSI6IjEiLCJ1c2VyTmFtZSI6ImZrYmd1d2VscnUiLCJleHAiOjE2ODMwMTI3OTQsInVzZXJJZCI6IjMifQ.7DiTFGh88ELyZyEJsXQs0gp1_1OyLKNNWr7f0sVKU28"
-        },
       userInfo: {},
       editInfoForm:{
         userName: "",
@@ -258,18 +240,6 @@ export default {
     reseteditPwdForm() {
       this.$refs.editPwdFormRef.resetFields();
     },
-     handleRemove(file, fileList) {
-        console.log(file, fileList);
-      },
-      handlePreview(file) {
-        console.log(file);
-      },
-      handleExceed(files, fileList) {
-        this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
-      },
-      beforeRemove(file, fileList) {
-        return this.$confirm(`确定移除 ${ file.name }？`);
-      }
   },
 };
 </script>
